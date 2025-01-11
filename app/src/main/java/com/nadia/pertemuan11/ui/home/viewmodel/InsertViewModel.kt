f707ab7ae1b5ad028f4c67d7aed100bc72fc1bdf
veresoora <lahanabira@gmail.com>
2025-01-11 22:54:11 +0700

    menambahkan sealed class FormState di InsertViewModel

---
+++ app/src/main/java/com/nadia/pertemuan11/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package com.nadia.pertemuan11.ui.home.viewmodel
+
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
