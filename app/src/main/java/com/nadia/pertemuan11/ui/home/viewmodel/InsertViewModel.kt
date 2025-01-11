0a14c324b666e74e18161decf87992cf6a1684ab
veresoora <lahanabira@gmail.com>
2025-01-11 22:59:03 +0700

    menambahkan data class Variabel yang menyimpan data input form

--- app/src/main/java/com/nadia/pertemuan11/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/nadia/pertemuan11/ui/home/viewmodel/InsertViewModel.kt
@@ -1,8 +1,20 @@
 package com.nadia.pertemuan11.ui.home.viewmodel
 
+import com.nadia.pertemuan11.model.Mahasiswa
+
 sealed class FormState {
     object Idle : FormState()
     object Loading : FormState()
     data class Success(val message: String) : FormState()
     data class Error(val message: String) : FormState()
 }
+
+//data class Variabel yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
